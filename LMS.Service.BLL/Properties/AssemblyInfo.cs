d5ebd4fc86ee1ae4c6f42410760082084bdd8028
Stayrony <beyoncem7@gmail.com>
2016-02-04 18:33:27 +0200

    clean up project

--- LMS.Service.BLL/Properties/AssemblyInfo.cs
+++ LMS.Service.BLL/Properties/AssemblyInfo.cs
@@ -7,18 +7,20 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
+
+
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("LMS.UI")]
+[assembly: AssemblyTitle("LMS.Service.BLL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LMS.UI")]
+[assembly: AssemblyProduct("LMS.Service.BLL")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -28,21 +30,8 @@ using System.Windows;
 // COM, задайте атрибуту ComVisible значение TRUE для этого типа.
 [assembly: ComVisible(false)]
 
-//Чтобы начать сборку локализованных приложений, задайте 
-//<UICulture>CultureYouAreCodingWith</UICulture> в файле .csproj
-//внутри <PropertyGroup>.  Например, если используется английский США
-//в своих исходных файлах установите <UICulture> в en-US.  Затем отмените преобразование в комментарий
-//атрибута NeutralResourceLanguage ниже.  Обновите "en-US" в
-//строка внизу для обеспечения соответствия настройки UICulture в файле проекта.
-
-//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, // где расположены словари ресурсов по конкретным тематикам
-    // (используется, если ресурс не найден на странице 
-    // или в словарях ресурсов приложения)
-    ResourceDictionaryLocation.SourceAssembly // где расположен словарь универсальных ресурсов
-    // (используется, если ресурс не найден на странице, 
-    // в приложении или в каких-либо словарях ресурсов для конкретной темы)
-    )]
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c3e0c9f1-d948-4c5b-a5b1-b65b882a2a84")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //      Основной номер версии
