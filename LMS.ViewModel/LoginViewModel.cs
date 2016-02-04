27c58d29d3ea41544ad553a89df7e5f9fa565d53
Stayrony <beyoncem7@gmail.com>
2016-02-04 18:32:36 +0200

    add LoginViewModel

---
+++ LMS.ViewModel/LoginViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.ViewModel
+{
+    public class LoginViewModel
+    {
+ /*       private bool Validate(RegisterInfo registerInfo)
+        {
+            foreach (string property in ValidatedProperties)
+            {
+                if (this.GetValidationError(property) != null)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        List<string> ValidatedProperties = new List<string>() { "Login", "Password" };
+
+        private string GetValidationError(string property)
+        {
+            string error = null;
+            switch (property)
+            {
+                case "Login":
+                    error = this.ValidateLogin();
+                    break;
+                case "Password":
+                    error = this.ValidatePassword();
+                    break;
+                default:
+                    break;
+            }
+
+            return error;
+        }
+  * */
+    }
+}
