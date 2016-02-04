5230b847e279146fbd48fe4bf2d2842295aa67aa
Stayrony <beyoncem7@gmail.com>
2016-02-04 18:30:18 +0200

    add Login and Register Info

---
+++ LMS.Service.Domain/LoginInfo.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LoginInfo.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The login info.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace LMS.Service.Domain
+{
+    /// <summary>
+    /// The login info.
+    /// </summary>
+    public class LoginInfo
+    {
+        /// <summary>
+        /// Gets or sets the login.
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
---
+++ LMS.Service.Domain/RegisterInfo.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RegisterInfo.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The register info.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace LMS.Service.Domain
+{
+    /// <summary>
+    /// The register info.
+    /// </summary>
+    public class RegisterInfo
+    {
+        /// <summary>
+        /// Gets or sets the login.
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
