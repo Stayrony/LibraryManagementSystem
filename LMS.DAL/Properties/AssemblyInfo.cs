145a0b051fac3aa32cb13e1a00813252645093d3
Stayrony <beyoncem7@gmail.com>
2016-02-04 18:31:09 +0200

    create UserDalManager

---
+++ LMS.DAL/Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("LMS.DAL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LMS.DAL")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("cda839b1-4a8e-46db-91b9-afcc4c6e8065")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LMS.DAL/UserDalManager.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UserDalManager.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The user dal manager.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace LMS.DAL
+{
+    using System;
+
+    using LMS.Service.Domain;
+
+    /// <summary>
+    ///     The user dal manager.
+    /// </summary>
+    public class UserDalManager
+    {
+        /// <summary>
+        /// The get user on login.
+        /// </summary>
+        /// <param name="login">
+        /// The login.
+        /// </param>
+        /// <returns>
+        /// The <see cref="User"/>.
+        /// </returns>
+        public User GetUserOnLogin(string login)
+        {
+            var _user = new User();
+
+            // TODO return user from DB
+            return _user;
+        }
+
+        /// <summary>
+        /// The create user.
+        /// </summary>
+        /// <param name="registerInfo">
+        /// The register info.
+        /// </param>
+        /// <returns>
+        /// The <see cref="User"/>.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public User CreateUser(RegisterInfo registerInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The delete user.
+        /// </summary>
+        /// <param name="login">
+        /// The login.
+        /// </param>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public void DeleteUser(string login)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
