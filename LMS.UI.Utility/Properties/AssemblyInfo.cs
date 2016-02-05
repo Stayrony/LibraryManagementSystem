aacd2115930963039b382e93da613ea89e880d85
Stayrony <beyoncem7@gmail.com>
2016-02-06 01:04:32 +0200

    add RelayCommand

---
+++ LMS.UI.Utility/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("LMS.UI.Utility")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LMS.UI.Utility")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("0eae19ee-b7ab-4e2e-8a90-8970f3af3a00")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LMS.UI.Utility/RelayCommand.cs
@@ -0,0 +1,105 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RelayCommand.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The relay command.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace LMS.UI.Utility
+{
+    using System;
+    using System.Diagnostics;
+    using System.Windows.Input;
+
+    /// <summary>
+    ///     The relay command.
+    /// </summary>
+    public class RelayCommand
+    {
+        /// <summary>
+        ///     The _can execute.
+        /// </summary>
+        private readonly Predicate<object> _canExecute;
+
+        /// <summary>
+        ///     The _execute.
+        /// </summary>
+        private readonly Action<object> _execute;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RelayCommand"/> class.
+        /// </summary>
+        /// <param name="execute">
+        /// The execute.
+        /// </param>
+        public RelayCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RelayCommand"/> class.
+        /// </summary>
+        /// <param name="execute">
+        /// The execute.
+        /// </param>
+        /// <param name="canExecute">
+        /// The can execute.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// </exception>
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// The can execute.
+        /// </summary>
+        /// <param name="parameter">
+        /// The parameter.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return this._canExecute == null ? true : this._canExecute(parameter);
+        }
+
+        /// <summary>
+        ///     The can execute changed.
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        /// <summary>
+        /// The execute.
+        /// </summary>
+        /// <param name="parameter">
+        /// The parameter.
+        /// </param>
+        public void Execute(object parameter)
+        {
+            this._execute(parameter);
+        }
+    }
+}
