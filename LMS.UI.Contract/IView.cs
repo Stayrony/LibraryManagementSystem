bf90416cde347c5a90057be96773ac60847aad63
Stayrony <beyoncem7@gmail.com>
2016-02-06 20:35:27 +0200

    add IView contract

---
+++ LMS.UI.Contract/IView.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IView.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The View interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace LMS.UI.Contract
+{    /// <summary>
+     ///     The View interface.
+     /// </summary>
+    public interface IView
+    {
+        /// <summary>
+        ///     The set focus.
+        /// </summary>
+        void SetFocus();
+
+        /// <summary>
+        /// The show error.
+        /// </summary>
+        /// <param name="error">
+        /// The error.
+        /// </param>
+        void ShowError(string error);
+
+    }
+}
