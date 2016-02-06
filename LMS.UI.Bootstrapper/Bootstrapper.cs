60889135be5385e7d8284f90c3261b8bc6defcf5
Stayrony <beyoncem7@gmail.com>
2016-02-06 23:25:16 +0200

    add Bootstrapper

---
+++ LMS.UI.Bootstrapper/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LMS.UI.Context;
+using LMS.UI.Control;
+
+namespace LMS.UI.Bootstrapper
+{
+    public class Bootstrapper
+    {
+        public Bootstrapper()
+        {
+            this.InitControls();
+        }
+
+        private void InitControls()
+        {
+          //  ControlManager.GetInstance().Add("LoginControl",typeof(LoginControl));
+        }
+    }
+}
